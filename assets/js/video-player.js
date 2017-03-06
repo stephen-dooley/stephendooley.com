f3a44df585844e498f065fd526993a69b5961aac
Stephen Dooley <sdooley@altocloud.com>
2017-03-06 22:03:18 +0000

    Add video player and replace contact form with technologies reference

---
+++ assets/js/video-player.js
@@ -0,0 +1,17 @@
+// This code loads the IFrame Player API code asynchronously.
+var tag = document.createElement('script');
+
+tag.src = "https://www.youtube.com/iframe_api";
+var firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+// This function creates an <iframe> (and YouTube player)
+// after the API code downloads.
+var player;
+function onYouTubeIframeAPIReady() {
+  player = new YT.Player('player', {
+    height: '400',
+    width: '300',
+    videoId: 'iww29-PK2R4'
+  });
+}
